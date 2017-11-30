3e527b793e2b1365dc4dfd45407fd4d809f72c2a
SantyAgu <32776322+SantyAgu@users.noreply.github.com>
2017-11-30 08:23:53 -0500

    Mock Usuarios

--- src/components/user-model/user-model.ts
+++ src/components/user-model/user-model.ts
@@ -3,6 +3,14 @@ export class UserModelComponent {
   public Username: string;
   public Password:string;
 
+  public static NewUser(_Username:string, _Password:string):UserModelComponent {
+    let _User:UserModelComponent = new UserModelComponent();
+    _User.Username = _Username;
+    _User.Password = _Password;
+    return _User;
+  }
+
+
   constructor() {
     console.log('Hello UserModelComponent Component');
     this.Username = null;
---
+++ src/providers/login-web/mock-usuarios.ts
@@ -0,0 +1,10 @@
+import {UserModelComponent} from "../../components/user-model/user-model";
+
+export const USUARIOS: UserModelComponent[]= [
+			UserModelComponent.NewUser("Santiago","Acn123"),
+			UserModelComponent.NewUser("Juan ","Acn123"),
+			UserModelComponent.NewUser("Cristian","Acn123"),
+			UserModelComponent.NewUser("Dianax2","Acn123"),
+			UserModelComponent.NewUser("Sandra","Acn123")
+            
+];
